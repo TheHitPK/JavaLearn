7f1b44615af6b2058d6461a64e3ed1dbbf5ddb0c
Pablo <pablot1804@gmail.com>
2024-04-21 22:58:16 -0400

    Primer commit Java

---
+++ src/holaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Pablo Cesar";
+		System.out.println(name);
+
+	}
+
+}
