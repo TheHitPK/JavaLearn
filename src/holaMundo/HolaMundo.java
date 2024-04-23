6cd8156bb470ec3864607816796b058d4fe58f1a
Pablo <pablot1804@gmail.com>
2024-04-22 20:41:55 -0400

    fix: ignoring post compiled files

--- src/holaMundo/HolaMundo.java
+++ src/holaMundo/HolaMundo.java
@@ -1,10 +1,11 @@
 package holaMundo;
-
+import java.util.Scanner;
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Pablo Cesar";
+		Scanner escaner = new Scanner(System.in);
+		String name = escaner.nextLine();
 		System.out.println(name);
 
 	}
