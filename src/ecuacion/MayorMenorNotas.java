bb4655a80b9eed42ff8ded503a0e915a945c820d
Pablo Laptop <pablot1804@gmail.com>
2024-04-30 22:43:35 -0400

    feature/ edad, materias y tipo de sexo

--- src/ecuacion/MayorMenorNotas.java
+++ src/ecuacion/MayorMenorNotas.java
@@ -1,5 +1,6 @@
 package ecuacion;
 import java.util.Scanner;
+import java.time.LocalDate;
 public class MayorMenorNotas {
 
 	public static void main(String[] args) {
@@ -14,6 +15,14 @@ public class MayorMenorNotas {
 		String mejorPromedio="";
 		String peorPromedio="";
 		int aux = 0;
+		int mayorEdad=0;
+		int menorEdad=0;
+		int masculino=0;
+		int femenino=0;
+		int programacion = 0;
+		int fisica=0;
+		int matematica=0;
+		int materia=0;
 		do {	
 			System.out.println("Cuantos estudiantes desea ingresar?");
 			int numStudent = scan.nextInt();
@@ -21,9 +30,86 @@ public class MayorMenorNotas {
 				System.out.println("Nombre del estudiante numero "+ (aux+1));
 				scan.nextLine();
 				String nameStudent = scan.nextLine();
+				//Fecha de nacimiendo del estudiante
+				System.out.println("Ingrese la fecha de nacimiento del estudiante del estudiante: ");
+				System.out.print("Ingrese el año: ");
+				int yearBorn= scan.nextInt();
+				System.out.print("Ingrese el mes: ");
+				int monthBorn= scan.nextInt();
+				System.out.print("Ingrese el dia: ");
+				int dayBorn= scan.nextInt();
+				
+				//Sexo del estudiante
+				System.out.println("Ingrese el tipo sexo del estudiante H/Hombre  M/Mujer");
+				String sex = scan.next();
+				switch(sex) {
+					case "H":
+						masculino++;
+					break;
+					
+					case "h":
+						masculino++;
+					break;
+					
+					case "M":
+						femenino++;
+					break;
+					
+					case "m":
+						femenino++;
+					break;
+					
+					default:
+						System.out.println("No binario");
+				}
+				
+				
+				//fecha actual en años, meses y dias
+				LocalDate today = LocalDate.now();
+				int yearDate = today.getYear();
+				int monthDate = today.getMonthValue();
+				int dayDate = today.getDayOfMonth();
+				int edad = 0;
+				
+				//Calculo de la edad
+				if(yearDate > yearBorn) {
+					edad = yearDate-yearBorn;
+					if(monthDate < monthBorn) {
+						edad--;
+					}else if(monthDate == monthBorn) {
+						if(dayDate < dayBorn) {
+							edad--;
+						}
+					}
+				}		 
+				int edadActual = edad;
+				if(edadActual >= 18) {
+					System.out.println("El estudiante es mayor de edad, tiene "+ edadActual);
+					 mayorEdad++;
+					
+				}else {
+					System.out.println("El estudiante es menor de edad, tiene "+ edadActual);
+					menorEdad--;
+				}
 				//numero de notas
 				System.out.println("Cantidad de notas del estudiante ");
 				int numRating = scan.nextInt();
+				System.out.println("Ingrese que materia esta cursando el estudiante:  1/programacion 2/fisica 3/Matematicas");
+				materia = scan.nextInt();
+				switch(materia) {
+					case 1:
+						programacion++;
+					break;
+					
+					case 2 :
+						fisica++;
+					break;
+					
+					case 3:
+						matematica++;
+					break;
+					
+				}
 				int promedio=0;
 				int notas = 0;
 				int sumNotas=0;
@@ -70,6 +156,14 @@ public class MayorMenorNotas {
 			System.out.println("Peor promedio: "+ peorPromedio +" con " + menorPromedio);
 			System.out.println("Mas notas procesadas: "+ masCantidadNotas +" con " +masNotas );
 			System.out.println("Menos notas procesadas: "+ menosCantidadNotas +" con " + menosNotas);
+			System.out.println("Cantidad de estudiantes mayores de edad: "+mayorEdad);
+			System.out.println("Cantidad de estudiantes menores de edad: "+menorEdad);
+			System.out.println("Cantidad de estudiantes masculinos: "+masculino);
+			System.out.println("Cantidad de estudiantes femeninas: "+femenino);
+			System.out.println("Cantidad de estudiantes cursando fisica: "+fisica);
+			System.out.println("Cantidad de estudiantes cursando programacion: "+programacion);
+			System.out.println("Cantidad de estudiantes cursando matematica: "+matematica);
+			
 			
 			System.out.println("Desea continuar? 1/si  0/no");
 			int opc= scan.nextInt();
