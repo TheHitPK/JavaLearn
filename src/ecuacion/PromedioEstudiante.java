182f988e77733fa5a8abb3e994002770267167b5
Pablo Laptop <pablot1804@gmail.com>
2024-05-01 21:51:43 -0400

    fix/ agregar notas

--- src/ecuacion/PromedioEstudiante.java
+++ src/ecuacion/PromedioEstudiante.java
@@ -10,6 +10,7 @@ public class PromedioEstudiante {
 		
 		//Ingreso de datos de estudiante
 		System.out.print("Ingrese el Nombre del estudiante: ");
+
 		String nameStudent = scan.nextLine();
 		System.out.print("Ingrese la Cedula del estudiante: ");
 		String dniStudent = scan.nextLine();
@@ -23,38 +24,26 @@ public class PromedioEstudiante {
 		LocalDate birthday = LocalDate.of(yearBorn, monthBorn, dayBorn);
 		
 		//fecha actual en años, meses y dias
-		LocalDate today = LocalDate.now();
-		//int yearDate = today.getYear();
-	//	int monthDate = today.getMonthValue();
-	//	int dayDate = today.getDayOfMonth();
-	//	int edad = 0;
+		LocalDate today = LocalDate.now();	
 		
+		//Calculo de edad
 		Period period = Period.between(birthday,today);
 		int year = period.getYears();
-		//Calculo de la edad
-		/*if(yearDate > yearBorn) {
-			edad = yearDate-yearBorn;
-			if(monthDate < monthBorn) {
-				edad--;
-			}else if(monthDate == monthBorn) {
-				if(dayDate < dayBorn) {
-					edad--;
-				}
-			}
-		}	*/	 
-		//int edadActual = edad;
+		
 		//Ingreso de notas
 		System.out.print("Ingrese las notas del estudiante: ");
+		int notas=0;
+		for(int i=0;i<3;i++) {
+			int nota = scan.nextInt();
+			if(nota >= 0 && nota<= 20) {
+				notas += nota;
+			}else {
+				System.out.println("Nota erronea");
+			}
+			
+		}
 		
-		int nota1= scan.nextInt();		
-		
-		if(nota1 >= 0 && nota1<= 20) {
-			int nota2= scan.nextInt();
-			if(nota2 >= 0 && nota2<= 20) {
-				int nota3= scan.nextInt();
-				scan.close();
-				if(nota3 >= 0 && nota3<= 20) {
-					int promedio = (nota1+nota2+nota3)/3;
+					int promedio = (notas)/3;
 					
 					System.out.println(nameStudent+" "+dniStudent);		
 					if(year >= 18) {
@@ -68,19 +57,6 @@ public class PromedioEstudiante {
 					}else {
 						System.out.println("El estudiante ha sido reprobado con un promedio de "+ promedio);
 					}
-					
-				}else{
-					System.out.println("Nota erronea");
-				}
-			}else{
-				System.out.println("Nota erronea");
-			}
-			
-		}else{
-			System.out.println("Nota erronea");
-		}
-		
-
 	}
 
 }
