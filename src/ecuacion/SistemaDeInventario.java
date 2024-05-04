c9be80ab99c9b284799e381e516d3aee4b26bc60
Pablo Laptop <pablot1804@gmail.com>
2024-05-04 14:41:04 -0400

    feature/ metodo agregarProductos, visualizarProductos, modificarProducto

--- src/ecuacion/SistemaDeInventario.java
+++ src/ecuacion/SistemaDeInventario.java
@@ -3,27 +3,77 @@ import java.util.Scanner;
 public class SistemaDeInventario {
 	static Scanner scan = new Scanner(System.in);
 	//variables
-	static int aux = 0;
+	static int cantTotalProductos = 0;
 	//arrays
-	static String producto[]=new String [10];
-	
-	
+	static String nombreProducto[] =new String [99];
+	static String codigoProducto[] = new String [99];
+	static double precioProducto[] = new double[99];
+	static int cantidadProducto[] = new int[99];
 	//metodos
+	//Metodo para agregar productos, ingresando nombre,cantidad,precio y codigo
 	static void agregarProducto(int cantProductos) {
-		for(int i = 0;i <=cantProductos; i++){
-			producto[aux]= scan.nextLine();
-			aux++;
+		for(int i =0;i <cantProductos; i++){
+			scan.nextLine();
+			System.out.println("Ingrese el nombre del producto");
+			nombreProducto[cantTotalProductos]= scan.nextLine();
+			System.out.println("Ingrese el codigo del producto");
+			String codigo= scan.nextLine();
+				for(int j=0;j<codigoProducto.length;j++){
+					if(codigo.equals(codigoProducto[j])) {
+						while(codigo.equals(codigoProducto[j])) {
+							System.out.println("Codigo repetido, ingrese un codigo valido");
+							codigo= scan.nextLine();
+						}						
+					}						
+				}		
+			codigoProducto[cantTotalProductos] = codigo;
+			System.out.println("Ingrese el precio del producto");
+			precioProducto[cantTotalProductos] = scan.nextDouble();
+			System.out.println("Ingrese la cantidad de articulos del producto ingresado");
+			cantidadProducto[cantTotalProductos] = scan.nextInt();			
+			cantTotalProductos++;
 		}
 		
 	}
-	
+	//Metodo para visualizar el inventario de productos
 	static void visualizarProductos(){
-		for(int i = 0; i<aux;i++) {
-			if(producto != null) {
-				System.out.println(producto[i]);
+		System.out.println("NOMBRE"+"\t"+"CODIGO"+"\t"+"PRECIO"+"\t"+"EXISTENCIA");
+		for(int i = 0; i<cantTotalProductos;i++) {			
+			if(nombreProducto != null) {				
+				System.out.println(nombreProducto[i]+"\t"+codigoProducto[i]+"\t"+precioProducto[i]+"\t"+cantidadProducto[i]);
+			}			
+		}
+	}
+	
+	static void modificarProductos() {
+		scan.nextLine();
+		System.out.println("Ingrese el codigo del producto que desea modificar");
+		String codigo = scan.nextLine();
+		for(int i = 0; i<codigoProducto.length; i++) {
+			if( codigo.equals(codigoProducto[i]) ) {
+				System.out.println("NOMBRE"+"\t"+"CODIGO"+"\t"+"PRECIO"+"\t"+"EXISTENCIA");
+				System.out.println(nombreProducto[i]+"\t"+codigoProducto[i]+"\t"+precioProducto[i]+"\t"+cantidadProducto[i]);
+				System.out.println("Que desea modificar?");
+				System.out.println("1- Nombre");
+				System.out.println("2- Precio");
+				System.out.println("3- Cantidad");
+				System.out.println("4- Salir");
+				int opcion = scan.nextInt();
+				scan.nextLine();
+				if(opcion == 1) {					
+					System.out.println("Ingrese el nuevo nombre del producto");
+					nombreProducto[i] = scan.nextLine();
+				} else if (opcion == 2) {
+					System.out.println("Ingrese el nuevo precio del producto");
+					precioProducto[i] = scan.nextDouble();
+				} else if (opcion == 3) {
+					System.out.println("Ingrese la nueva cantidad de articulos en existencia del producto");
+					cantidadProducto[i] = scan.nextInt();
+				} else if (opcion == 4) {
+				} else {
+					System.out.println("Error- ingrese una opcion valida");
+				}
 			}
-			
-			
 		}
 	}
 
@@ -38,10 +88,9 @@ public class SistemaDeInventario {
 			System.out.println("4- Estadistica.");
 			System.out.println("5- Configuraciones (IVA / TASA DE CAMBIO).");
 			System.out.println("6- Salir.");
-			int opc= 0;
-			opc = scan.nextInt();
-			//MENU PRINCIPAL
+			int opc= scan.nextInt();
 			
+			//MENU PRINCIPAL			
 			if(opc==1) {
 				System.out.println("Agregar producto.");
 				System.out.println("Ingrese la cantidad de productos que sea agregar");
@@ -52,7 +101,7 @@ public class SistemaDeInventario {
 				System.out.println(" Visualizar Productos.");
 				visualizarProductos();
 			}else if(opc == 3) {
-				
+				modificarProductos();
 			}else if(opc == 4) {
 				
 			}else if(opc == 5) {
