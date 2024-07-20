92fb548c5659499e63b57a303a96412809b0635f
Dazt5 <alvarezdavidt15@gmail.com>
2024-07-20 11:53:50 -0500

    mock

--- src/pokedexPackage/Tabla.java
+++ src/pokedexPackage/Tabla.java
@@ -1,52 +1,58 @@
 package pokedexPackage;
 
-import java.awt.EventQueue;
-
+import java.awt.LayoutManager;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
 
-public class Tabla extends JFrame {
+public class Tabla {
 
-	private static final long serialVersionUID = 1L;
-	private static JPanel contentPane;
-	// private JTable table;
+    //Datos de prueba, podria tener aqui una clase que me los de, clase la cual podria consultar una base de datos.
+    private static final String[] HEADERS = new String[]{"header1", "header2"};
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					JFrame frame = new JFrame("xd");
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setBounds(100, 100, 450, 300);
-					contentPane = new JPanel();
-					contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    private static final String[][] DATOS_DE_TABLA = new String[][]{
+            {"hola", " hola otra vez"},
+            {"ola ola", "jelou jelou"}
+    };
 
-					frame.setContentPane(contentPane);
-					contentPane.setLayout(null);
+    private static final String[][] OTROS_DATOS_PARA_OTRA_TABLA = new String[][]{
+            {"nuevos", " datos"},
+            {"para", "comparar"}
+    };
 
-					String[] filas = { "uno", "Dos" };
-					Object[][] columnas = { { "ejje", "jaja" }, { "Probando", "pRODANB" } };
-					DefaultTableModel modelo = new DefaultTableModel(columnas, filas);
-					JTable table = new JTable(modelo);
-					JScrollPane scroll = new JScrollPane(table);
-					table.setBounds(54, 42, 312, 154);
-					contentPane.add(table);
-					frame.getContentPane().add(scroll);
-					frame.setVisible(true);
 
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        JFrame principalPanel = createPrincipalFrame(1024, 1000);
+
+        JPanel panelWithData = createPanel(null);
+        JScrollPane tablaDatosPrimerTrimestre = createTableWithScroll(DATOS_DE_TABLA, HEADERS, 100, 200, 200, 300);
+        JScrollPane tablaDatosSegundoTrimestre  = createTableWithScroll(OTROS_DATOS_PARA_OTRA_TABLA, HEADERS, 600, 200, 200, 300);
+
+        panelWithData.add(tablaDatosPrimerTrimestre);
+        panelWithData.add(tablaDatosSegundoTrimestre);
+
+        principalPanel.add(panelWithData);
+        principalPanel.setVisible(true);
+    }
+
+    private static JFrame createPrincipalFrame(int width, int height) {
+        JFrame principalPanel = new JFrame("frame principal");
+        principalPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        principalPanel.setSize(width, height);
+        return principalPanel;
+    }
+
+    private static JPanel createPanel(LayoutManager layoutManager) {
+        return new JPanel(layoutManager);
+    }
+
+    private static JScrollPane createTableWithScroll(String[][] data, String[] headers, int x, int y, int width, int height){
+        JTable jTable = new JTable(data, headers);
+        JScrollPane scrollPane = new JScrollPane(jTable);
+        scrollPane.setBounds(x, y, width, height);
+        return scrollPane;
+    }
+
 
 }
